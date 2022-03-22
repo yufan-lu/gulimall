27b235c7ca07d0432301a4c313772822d3d75703
yufan-lu <yufanlu98@outlook.com>
2022-03-21 23:59:02 -0700

    init

---
+++ gulimall-ware/src/test/java/com/lyf/gulimall/lware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyf.gulimall.lware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
